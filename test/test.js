8e330478b19711adc5128a2a5dba87905295ddb7
yexi <xuefan.xf@alibaba-inc.com>
2020-04-27 15:25:02 +0800

    fix :  find n bug

--- test/test.js
+++ test/test.js
@@ -4,26 +4,44 @@
 // js
 const GG = require('../main');
 const code = `
-    const g = 'Hello World';
+    const g:string = 'Hello World';
+    export const a = 1;
+
+    const func = () => {
+        const c = 1;
+    }
+    
 `
 const AST = GG.createAstObj(code);
-const { nodePathList, matchWildCardList } = AST.getAstsBySelector(`const $_$ = $_$`);
-console.log(nodePathList, matchWildCardList);
 
-AST.replaceSelBySel(`const $_$ = $_$`, `const join = function () {
-    return $_$ + $_$
-}`);
-
-console.log(AST.generate());
-
-const newAst = GG.buildAstByAstStr(`
-    function g () {
-        if (true) {
-            '$_$body$_$'
-        }
+const { nodePathList } = AST.getAstsBySelector(`const $_$ = $_$`, true, 'n');
+nodePathList.forEach(n => {
+    if (n.parent.parent.node.type == 'ExportNamedDeclaration') {
+        return;
     }
-`, {
-    body: nodePathList[0].node
+    GG.replaceAstByAst(n.parent, { type: 'ExportNamedDeclaration', declaration: n.parent.value})
 })
+console.log(AST.generate())
 
-console.log(GG.generate(newAst))
+// const { nodePathList, matchWildCardList } = AST.getAstsBySelector(`const $_$ = $_$`);
+// console.log(nodePathList, matchWildCardList);
+
+// AST.replaceSelBySel(`const $_$ = $_$`, `const join = function () {
+//     return $_$ + $_$
+// }`);
+
+// console.log(AST.generate());
+
+// const newAst = GG.buildAstByAstStr(`
+//     function g () {
+//         if (true) {
+//             '$_$body$_$'
+//         }
+//     }
+// `, {
+//     body: nodePathList[0].node
+// })
+
+
+
+// console.log(GG.generate(newAst))
--- test/visit.js
+++ test/visit.js
@@ -7,29 +7,38 @@ const code = `
     module.exports = function() {
         const g = 'Hello World';
         var c = a.b();
+        var d = a;
+        var e = a.b.c.d(f.e());
         this.dd
         function test() {
             console.log(this.test);
         }
+        let lll = 1;
     }
 `
 const ast = GG.createAstObj(code);
+const { nodePathList: p1, matchWildCardList: e1 } = ast.getAstsBySelector(`$_$`);
+const { nodePathList: p2, matchWildCardList: e2 } = ast.getAstsBySelector(`$_$.$_$`, false);
+p1.forEach((p, index) => {
+    const e = e1[index];
+    console.log(`${e[0].value}----${p.name}`)
+})
 const { nodePathList, matchWildCardList } = ast.getAstsBySelector({ type: 'ThisExpression'});
 ast.visit({
     visitMemberExpression(path) {
-        console.log(path.node)
+        // console.log(path.node)
         this.traverse(path);
     },
     visitThisExpression(path) {
-        console.log(path.node)
+        // console.log(path.node)
         this.abort();
     },
 
     visitFunction(path) {
-        console.log(path.node)
+        // console.log(path.node)
         return false;
     }
 });
 ast.traverse(node => {
-    console.log(node);
+    // console.log(node);
 })
